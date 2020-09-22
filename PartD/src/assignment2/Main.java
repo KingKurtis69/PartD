b6cd600fa0996a3d6287f45640b27544e3937e8b
KingKurtis69 <smithgaming69@gmail.com>
2020-09-23 01:28:07 +1200

    added first line of code that displays my name

--- PartD/src/assignment2/Main.java
+++ PartD/src/assignment2/Main.java
@@ -3,8 +3,7 @@ package assignment2;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hi, my name is Kurtis Smith");
-
+		System.out.println("Hi, my name is Kurtis Smith!");
 	}
 
 }
