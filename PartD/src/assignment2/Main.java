8ca91568bc836716fd21a8b2bcfae16daae90c43
KingKurtis69 <smithgaming69@gmail.com>
2020-09-23 01:31:52 +1200

    Commiting change for code with favourite colour

--- PartD/src/assignment2/Main.java
+++ PartD/src/assignment2/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Kurtis Smith!");
+		
+		System.out.println("My foavourite colour is Blue!");
 	}
 
 }
